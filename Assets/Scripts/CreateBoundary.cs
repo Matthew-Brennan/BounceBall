bce68e598886858847ff8fac64e4a288f3c240ac
Matthew-Brennan <32656444+Matthew-Brennan@users.noreply.github.com>
2022-08-26 15:23:15 -0400

    commit

--- Assets/Scripts/CreateBoundary.cs
+++ Assets/Scripts/CreateBoundary.cs
@@ -18,10 +18,10 @@ public class CreateBoundary : MonoBehaviour
         Vector3 point = new Vector3();
 
         //place right
-        point = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width + 40, 0, Camera.main.nearClipPlane));
+        point = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, Camera.main.nearClipPlane));
         rightBoundary.transform.position = point;
 
-        point = Camera.main.ScreenToWorldPoint(new Vector3(-40, 0, Camera.main.nearClipPlane));
+        point = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, Camera.main.nearClipPlane));
         leftBoundary.transform.position = point;
 
     }
